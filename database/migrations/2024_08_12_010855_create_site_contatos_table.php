e1ec292a1195ef000805fc6adaf82857a4df4d26
jessejunior02 <jessepcarvalhojunior02@gmail.com>
2024-08-11 23:32:50 -0300

    Implementando a migration SiteContato

--- database/migrations/2024_08_12_010855_create_site_contatos_table.php
+++ database/migrations/2024_08_12_010855_create_site_contatos_table.php
@@ -16,6 +16,11 @@ class CreateSiteContatosTable extends Migration
         Schema::create('site_contatos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('nome', 180);
+            $table->string('telefone', 20);
+            $table->string('email', 80);
+            $table->integer('motivo_contato');
+            $table->text('mensagem');
         });
     }
 
